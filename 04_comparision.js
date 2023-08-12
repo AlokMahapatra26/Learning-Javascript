2b1d4af91dd062f6e272fe18ed749987691c900c
AlokMahapatra26 <alokmahapatra2604@gmail.com>
2023-08-13 01:23:36 +0530

    04_comparision

---
+++ 04_comparision.js
@@ -0,0 +1,28 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+// console.log(2 == 1);
+
+//results 
+// true
+// false
+// true
+// false
+// true
+// false
+
+
+// console.log(2 < '02'); => true
+// console.log(null >= 0)
+
+// console.log(undefined > 0) => fasle
+// console.log(undefined == 0) => false
+// console.log(undefined >= 0) => false
+
+
+// ===
+// console.log('2' == 2); => true
+// console.log('2' === 2); => false
+
