8cfecea9f5cb58e42909a748ba9442bf20b6f2d8
AlokMahapatra26 <alokmahapatra2604@gmail.com>
2023-08-21 08:05:53 +0530

    leant about array

---
+++ basics2/08_array.js
@@ -0,0 +1,34 @@
+
+// const myArr = [0,1,2,3,4,5,true,null,undefined,"Alok"];
+// console.log(myArr)
+// console.log(myArr.length)
+
+//Array method
+const arr = ["aarti","sagaraika","gouri"];
+// console.log(arr); 
+
+// arr.push("Alok");//add from last
+// console.log(arr); 
+
+// arr.pop();//remove from last (return value)
+// console.log(arr); 
+
+// arr.shift();//remove from first (return value)
+// console.log(arr);
+
+// arr.unshift("Alok");//add from first 
+// console.log(arr);
+
+// const newArr = arr.join(); //convert all value to string
+// console.log(newArr)
+
+// console.log(arr.includes("aarti"));
+// console.log(arr.indexOf("sagaraika"));
+
+// console.log("A " , arr);
+// console.log( arr.slice(0,1)); //deep copy
+// console.log("B " , arr)
+
+// console.log("A " , arr);
+// console.log( arr.splice(0,1)); //shallow copy
+// console.log("B " , arr)
